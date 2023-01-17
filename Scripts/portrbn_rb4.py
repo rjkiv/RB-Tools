64c51e613a7865c93a64da1a51537bde7ebca745
rjkiv <76180273+rjkiv@users.noreply.github.com>
2023-01-16 17:12:05 -0700

    RB4 RBN ports now support songs with harmonies

--- Scripts/portrbn_rb4.py
+++ Scripts/portrbn_rb4.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from mido import MidiFile, MidiTrack
 import sys
 import pprint
     
@@ -28,13 +29,54 @@ def port_rbn_rb4():
                     break
             for g in cwd.glob(f"songs/{shortname_ugc}/*"):
                 if shortname_new in g.name:
-                    print(g.name)
-                    # if there are harmonies...
+                    harms_in_mid = False
+                    mid = MidiFile(g)
+                    for track in mid.tracks:
+                        if "HARM" in track.name:
+                            harms_in_mid = True
+                            break
+                    # if there are harmonies:
+                    if harms_in_mid:
+                        # create two mids:
+                            # the first is the original RB4 mid but with harms removed
+                            # the second is just pro drums and harms, meant to be an _update.mid file
+                            mid_for_con = mid
+                            remove_from_con_mid = []
+                            mid_for_update = MidiFile()
+                            for track in mid_for_con.tracks:
+                                if track.name == shortname_new: # the tempo map
+                                    mid_for_update.add_track()
+                                    mid_for_update.tracks[-1] = track.copy()
+                                elif track.name == "PART DRUMS":
+                                    mid_for_update.add_track()
+                                    mid_for_update.tracks[-1] = track.copy()
+                                elif "HARM" in track.name:
+                                    mid_for_update.add_track()
+                                    mid_for_update.tracks[-1] = track.copy()
+                                    remove_from_con_mid.append(track)
+                            for track in remove_from_con_mid:
+                                mid_for_con.tracks.remove(track)
+                                
+#                            # just to verify that the two mids check out:
+#                            print("new RB4 mid:")
+#                            for track in mid_for_con.tracks:
+#                                print(track.name)
+#                            print("\nupdate mid:")
+#                            for track in mid_for_update.tracks:
+#                                print(track.name)
+                                
+                            mid_for_con.save(output_path.joinpath(f"{shortname_new}.mid"))
+                            mid_for_update.save(output_path.joinpath(f"{shortname_new}_update.mid"))
+                            print("base and update mids created")
+                            
                     # else, just stick the mid as is into the output folder
-                    output_path.joinpath(g.name).write_bytes(g.read_bytes())
+                    else:
+                        output_path.joinpath(g.name).write_bytes(g.read_bytes())
+                        print("base mid created")
                 # rename/move the .mogg
                 elif ".mogg" in g.name:
                     output_path.joinpath(f"{shortname_new}.mogg").write_bytes(g.read_bytes())
+                    print(f"{g.name} --> {shortname_new}.mogg")
                 # rename/move the .milo/.png/weights files
                 elif g.name == "gen":
                     for h in cwd.glob(f"songs/{shortname_ugc}/gen/*"):
@@ -58,12 +100,6 @@ def main():
     # and stick the corresponding RB4 song mid in the same folder as the original song's mid,
     # and the script will create an "output" folder containing the updated files to create a new CON with.
     port_rbn_rb4()
-    
-    # FIXME: add support for the comments below
-    # for the purposes of RB3DX, if harmonies are found in the RB4 mid, make an _update.mid and a line for missing_songs.dta, and put these in another folder titled "RB3DX"
-    # AND delete the harmonies from the "output" mid
-    
-    # the _update.mid should contain pro drums and harms
     
 if __name__ == "__main__":
     main()
