0e3c363d90b624335076805c6968cb0b73604d47
rjkiv <rjkiv@users.noreply.github.com>
2023-01-23 11:37:11 -0700

    dict to dta list implemented

--- Scripts/dict_to_dta.py
+++ Scripts/dict_to_dta.py
@@ -137,6 +137,25 @@ def dict_to_dta(song_dict: dict, indent=0):
             else:
                 print(f"{'    '*(indent)}({song_k} {song_v})")
 
+def dict_to_dta_list(song_dict: dict, indent=0):
+    lines = []
+    for song_k,song_v in song_dict.items():
+        if isinstance(song_v, dict):
+            lines.append(f"{'    '*indent}({song_k}")
+            lines.extend(dict_to_dta_list(song_v, indent+1))
+            lines.append(f"{'    '*indent})")
+        else:
+            if isinstance(song_v, str) and song_k in ["name", "artist", "album_name", "midi_file"]:
+                lines.append(f"{'    '*(indent)}({song_k} \"{song_v}\")")
+            elif isinstance(song_v, list):
+                if song_k == "preview":
+                    lines.append(f"{'    '*(indent)}({song_k} {' '.join(str(a) for a in song_v)})")
+                else:
+                    lines.append(f"{'    '*(indent)}({song_k} ({' '.join(str(a) for a in song_v)}))")
+            else:
+                lines.append(f"{'    '*(indent)}({song_k} {song_v})")
+    return lines
+
 def main():
     if len(sys.argv) != 2:
         print("no dta provided")
@@ -145,6 +164,10 @@ def main():
 
     print("Dict to dta...")
     dict_to_dta(big_song_dict["songs"])
+
+    # print(dict_to_dta_list(big_song_dict["songs"]))
+    for line in dict_to_dta_list(big_song_dict["songs"]):
+        print(line)
 
     
 if __name__ == "__main__":
